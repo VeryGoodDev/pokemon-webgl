db7581e6e9d21dec5c1d25886a67bcd025705fd8
VeryGoodDev <very_good_dev@outlook.com>
2021-12-04 01:22:49 -0800

    Started redoing texture abstraction, didn't get far
    (because I'm going to bed, I can continue on this tomorrow)

--- src/textures.ts
+++ src/textures.ts
@@ -1,1 +1,11 @@
-export function drawTexture(): void {}
+import ShaderProgram from './shaders/ShaderProgram'
+
+export default class Texture {
+  #shaderInfo: ShaderProgram
+
+  constructor(shaderInfo: ShaderProgram) {
+    this.#shaderInfo = shaderInfo
+  }
+
+  // draw(): void {}
+}
