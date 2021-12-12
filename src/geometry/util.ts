f2f28dd6ee1b044d446ff6be19f9b716bfb084b8
VeryGoodDev <very_good_dev@outlook.com>
2021-12-11 23:29:17 -0800

    Add a little whitespace for readability

--- src/geometry/util.ts
+++ src/geometry/util.ts
@@ -5,9 +5,11 @@ export function getRectangleBufferData(position: Vec2, size: Size, mirrorX = fal
   const x2 = x1 + size.width
   const y1 = position.y
   const y2 = y1 + size.height
+
   const xStart = mirrorX ? x2 : x1
   const xEnd = mirrorX ? x1 : x2
   const yStart = mirrorY ? y2 : y1
   const yEnd = mirrorY ? y1 : y2
+
   return [xStart, yStart, xEnd, yStart, xStart, yEnd, xStart, yEnd, xEnd, yStart, xEnd, yEnd]
 }
