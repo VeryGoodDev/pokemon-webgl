193066040d01d931986f4346bbc4b1cf087b929e
VeryGoodDev <very_good_dev@outlook.com>
2021-12-12 01:03:48 -0800

    Empty class with a shit ton of notes for later

---
+++ src/scenes/SceneManager.ts
@@ -0,0 +1,10 @@
+// TODO: draw method that draws two layers: background layer (with the background image of the scene, different from the background layer for solid colors) and entity layer on top of that, drawing all entites that are currently visible
+// TODO: Only draw entites that are within view of the camera (centered on the player usually), plus a little leeway to allow moving entities that could potentially move into view to continue updating
+// TODO: Track entities, update per scene change
+// TODO: Handle scene changes, including background swap, entrance point, exit points, etc.
+// TODO: Sets up scene collision boundaries
+// TODO: Determine background color, provide method to get that info (will usually be the out-of-bounds gray AFAIK)
+
+class SceneManager {}
+
+export default SceneManager
